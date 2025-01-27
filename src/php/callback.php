6b201e848585075f5e12e6165b6cb8f3130669e8
spkide <sundepressivo@gmail.com>
2025-01-27 14:11:30 -0600

    actualizacion-49

--- src/php/callback.php
+++ src/php/callback.php
@@ -2,8 +2,8 @@
 require 'vendor/autoload.php';
 
 $client = new Google_Client();
-$client->setClientId('TU_CLIENT_ID');
-$client->setClientSecret('TU_CLIENT_SECRET');
+$client->setClientId('1079407262165-qhh0cg8cidi37or7e9p9dghrqft92n7q.apps.googleusercontent.com');
+$client->setClientSecret('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');
 $client->setRedirectUri('http://localhost/callback.php'); // Cambia esto a tu URI de redirección
 
 if (isset($_GET['code'])) {
